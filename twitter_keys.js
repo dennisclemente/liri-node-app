245744fecc63e650fab7ab9e1d8245f79da29a50
Dennis Clemente <DennisClemente@gmail.com>
2017-09-28 16:45:18 -0700

    keys

---
+++ twitter_keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+	consumer_key: 		'jmMY4vli6FC9chD0zvBM0Cqsf',
+  	consumer_secret: 		'F4uZitIm6ZC8l4H1oopxFVCCvtJkkiWG9BAoRmdvT3S0tOoNTp',
+  	access_token_key: 	'912771666047938560-izWRtkkhy4RGQcY6yxkQiMgbVsK3cUx',
+  	access_token_secret: 	'mI6ncU5yMePaJi3jdM7EThRT8trYq2CZTjM5wsYnbv2N2',
+}
